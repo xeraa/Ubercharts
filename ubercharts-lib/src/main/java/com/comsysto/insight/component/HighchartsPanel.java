ff275d2c7b5ce7995df009d077639c9ee623023e
Philipp Krenn <pk@xeraa.net>
2012-02-11 18:56:46 +0100

    UPDATED to the latest jQuery version (1.7.1)

--- ubercharts-lib/src/main/java/com/comsysto/insight/component/HighchartsPanel.java
+++ ubercharts-lib/src/main/java/com/comsysto/insight/component/HighchartsPanel.java
@@ -29,46 +29,46 @@ import com.comsysto.insight.model.Highchart;
 public class HighchartsPanel extends Panel implements IHeaderContributor {
 	private static final long serialVersionUID = -4334873010475917041L;
 
-  public HighchartsPanel(String id, final Highchart highcharts) {
-    super(id);
+	public HighchartsPanel(String id, final Highchart highcharts) {
+		super(id);
 
-    final WebMarkupContainer chartDiv = new WebMarkupContainer("highchart");
-    chartDiv.setOutputMarkupId(true);
+		final WebMarkupContainer chartDiv = new WebMarkupContainer("highchart");
+		chartDiv.setOutputMarkupId(true);
 
-    add(chartDiv);
+		add(chartDiv);
 
-    // store ID into chart.renderTo
-    highcharts.getChart().setRenderTo(chartDiv.getMarkupId());
+		// store ID into chart.renderTo
+		highcharts.getChart().setRenderTo(chartDiv.getMarkupId());
 
-    /*
+		/*
 		 * we inject the script in the component body and not as a header
 		 * contribution because the script needs to be called each time the
 		 * component is refreshed using wicket ajax support.
-    */
-    add(new Label("script", new AbstractReadOnlyModel<String>() {
+		 */
+		add(new Label("script", new AbstractReadOnlyModel<String>() {
 			private static final long serialVersionUID = 7695732603979024344L;
 
-      @Override
-      public String getObject() {
-        StringBuffer js = new StringBuffer();
-        js.append("var ").append(chartDiv.getMarkupId()).append(";\n");
-        js.append("$(document).ready(function() {\n");
-        js.append(chartDiv.getMarkupId());
-        js.append(" = new Highcharts.Chart(");
+			@Override
+			public String getObject() {
+				StringBuffer js = new StringBuffer();
+				js.append("var ").append(chartDiv.getMarkupId()).append(";\n");
+				js.append("$(document).ready(function() {\n");
+				js.append(chartDiv.getMarkupId());
+				js.append(" = new Highcharts.Chart(");
 
-        js.append(highcharts.toJson());
+				js.append(highcharts.toJson());
 
-        js.append(" ); }); ");
+				js.append(" ); }); ");
 
-        return js.toString();
-      }
-    }).setEscapeModelStrings(false));
-  }
+				return js.toString();
+			}
+		}).setEscapeModelStrings(false));
+	}
 
-  public void renderHead(IHeaderResponse response) {
-    response.renderJavascriptReference(new ResourceReference(this.getClass(), "jquery.min.js"));
-    response.renderJavascriptReference(new ResourceReference(this.getClass(), "highcharts.js"));
-    response.renderJavascriptReference(new ResourceReference(this.getClass(), "exporting.js"));
-  }
+	public void renderHead(IHeaderResponse response) {
+		response.renderJavascriptReference(new ResourceReference(this.getClass(), "jquery.min.js"));
+		response.renderJavascriptReference(new ResourceReference(this.getClass(), "highcharts.js"));
+		response.renderJavascriptReference(new ResourceReference(this.getClass(), "exporting.js"));
+	}
 
 }
