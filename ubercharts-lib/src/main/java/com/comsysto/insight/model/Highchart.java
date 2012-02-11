faf0336ff5d05ff4432e1445263b089e47cbcbbd
Philipp Krenn <pk@xeraa.net>
2012-02-12 00:39:03 +0100

    MINOR code enhancement

--- ubercharts-lib/src/main/java/com/comsysto/insight/model/Highchart.java
+++ ubercharts-lib/src/main/java/com/comsysto/insight/model/Highchart.java
@@ -16,239 +16,249 @@
 
 package com.comsysto.insight.model;
 
-import com.comsysto.insight.model.charts.Chart;
-import com.comsysto.insight.model.options.*;
-import com.comsysto.insight.model.options.series.generic.ISeries;
-import org.codehaus.jackson.annotate.JsonProperty;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.annotate.JsonSerialize;
-
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import com.comsysto.insight.model.charts.Chart;
+import com.comsysto.insight.model.options.Axis;
+import com.comsysto.insight.model.options.ChartLabels;
+import com.comsysto.insight.model.options.ChartTitle;
+import com.comsysto.insight.model.options.Credits;
+import com.comsysto.insight.model.options.Exporting;
+import com.comsysto.insight.model.options.Legend;
+import com.comsysto.insight.model.options.Loading;
+import com.comsysto.insight.model.options.Navigation;
+import com.comsysto.insight.model.options.Point;
+import com.comsysto.insight.model.options.Subtitle;
+import com.comsysto.insight.model.options.Symbol;
+import com.comsysto.insight.model.options.Tooltip;
+import com.comsysto.insight.model.options.series.generic.ISeries;
+
 public class Highchart {
 
-    private ObjectMapper mapper = new ObjectMapper();
+	private ObjectMapper mapper = new ObjectMapper();
 
-    private Chart chart;
-    private String[] colors;
-    private Credits credits;
-    private ChartLabels labels;
-    private Legend legend;
-    private Loading loading;
-    private Point point;
-    private List<ISeries> series = new LinkedList<ISeries>();
-    private Subtitle subtitle;
-    private String[] symbols;
-    private ChartTitle title;
-    private Tooltip tooltip;
-    private Axis xAxis = new Axis();
-    private Axis yAxis = new Axis();
-    private Exporting exporting;
-    private Navigation navigation;
+	private Chart chart;
+	private String[] colors;
+	private Credits credits;
+	private ChartLabels labels;
+	private Legend legend;
+	private Loading loading;
+	private Point point;
+	private List<ISeries<?>> series = new LinkedList<ISeries<?>>();
+	private Subtitle subtitle;
+	private String[] symbols;
+	private ChartTitle title;
+	private Tooltip tooltip;
+	private Axis xAxis = new Axis();
+	private Axis yAxis = new Axis();
+	private Exporting exporting;
+	private Navigation navigation;
 
-    public Highchart() {
-        mapper.getSerializationConfig().setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
-    }
+	public Highchart() {
+		mapper.getSerializationConfig().setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
+	}
 
-    public Highchart(Chart pChart, ISeries... pSeries) {
-        this();
-        chart = pChart;
-        setSeries(pSeries);
-    }
+	public Highchart(Chart pChart, ISeries<?>... pSeries) {
+		this();
+		chart = pChart;
+		setSeries(pSeries);
+	}
 
-    public Highchart(Chart pChart, List<ISeries> pSeries) {
-        this(pChart);
-        setSeries(pSeries);
-    }
+	public Highchart(Chart pChart, List<ISeries<?>> pSeries) {
+		this(pChart);
+		setSeries(pSeries);
+	}
 
+	public String toJson() {
 
-    public String toJson() {
+		String json = "";
 
-        String json = "";
+		try {
+			json = mapper.writeValueAsString(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        try {
-            json = mapper.writeValueAsString(this);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		// dump JSON object
+		System.out.println(json);
 
-        // dump JSON object
-        System.out.println(json);
+		return json;
+	}
 
-        return json;
-    }
+	public Highchart addSeries(ISeries<?>... pSeries) {
 
+		for (ISeries<?> s : pSeries) {
+			series.add(s);
+		}
 
-    public Highchart addSeries(ISeries... pSeries) {
+		return this;
+	}
 
-        for (ISeries s : pSeries) {
-            series.add(s);
-        }
+	public Highchart setSeries(ISeries<?>... pSeries) {
+		series.clear();
+		addSeries(pSeries);
+		return this;
+	}
 
-        return this;
-    }
+	public Chart getChart() {
+		return chart;
+	}
 
-    public Highchart setSeries(ISeries... pSeries) {
-        series.clear();
-        addSeries(pSeries);
-        return this;
-    }
+	public Highchart setChart(Chart pChart) {
+		chart = pChart;
+		return this;
+	}
 
-    public Chart getChart() {
-        return chart;
-    }
+	public String[] getColors() {
+		return colors;
+	}
 
-    public Highchart setChart(Chart pChart) {
-        chart = pChart;
-        return this;
-    }
+	public Highchart setColors(String[] pColors) {
+		colors = pColors;
+		return this;
+	}
 
-    public String[] getColors() {
-        return colors;
-    }
+	public Credits getCredits() {
+		return credits;
+	}
 
-    public Highchart setColors(String[] pColors) {
-        colors = pColors;
-        return this;
-    }
+	public Highchart setCredits(Credits pCredits) {
+		credits = pCredits;
+		return this;
+	}
 
-    public Credits getCredits() {
-        return credits;
-    }
+	public ChartLabels getLabels() {
+		return labels;
+	}
 
-    public Highchart setCredits(Credits pCredits) {
-        credits = pCredits;
-        return this;
-    }
+	public Highchart setLabels(ChartLabels pLabels) {
+		labels = pLabels;
+		return this;
+	}
 
-    public ChartLabels getLabels() {
-        return labels;
-    }
+	public Legend getLegend() {
+		return legend;
+	}
 
-    public Highchart setLabels(ChartLabels pLabels) {
-        labels = pLabels;
-        return this;
-    }
+	public Highchart setLegend(Legend pLegend) {
+		legend = pLegend;
+		return this;
+	}
 
-    public Legend getLegend() {
-        return legend;
-    }
+	public Loading getLoading() {
+		return loading;
+	}
 
-    public Highchart setLegend(Legend pLegend) {
-        legend = pLegend;
-        return this;
-    }
+	public Highchart setLoading(Loading pLoading) {
+		loading = pLoading;
+		return this;
+	}
 
-    public Loading getLoading() {
-        return loading;
-    }
+	public Point getPoint() {
+		return point;
+	}
 
-    public Highchart setLoading(Loading pLoading) {
-        loading = pLoading;
-        return this;
-    }
+	public Highchart setPoint(Point pPoint) {
+		point = pPoint;
+		return this;
+	}
 
-    public Point getPoint() {
-        return point;
-    }
+	public List<ISeries<?>> getSeries() {
+		return series;
+	}
 
-    public Highchart setPoint(Point pPoint) {
-        point = pPoint;
-        return this;
-    }
+	public Highchart setSeries(List<ISeries<?>> pSeries) {
+		series = pSeries;
+		return this;
+	}
 
-    public List<ISeries> getSeries() {
-        return series;
-    }
+	public Subtitle getSubtitle() {
+		return subtitle;
+	}
 
-    public Highchart setSeries(List<ISeries> pSeries) {
-        series = pSeries;
-        return this;
-    }
+	public Highchart setSubtitle(Subtitle pSubtitle) {
+		subtitle = pSubtitle;
+		return this;
+	}
 
-    public Subtitle getSubtitle() {
-        return subtitle;
-    }
+	public String[] getSymbols() {
+		return symbols;
+	}
 
-    public Highchart setSubtitle(Subtitle pSubtitle) {
-        subtitle = pSubtitle;
-        return this;
-    }
+	public Highchart setSymbols(String[] pSymbols) {
+		symbols = pSymbols;
+		return this;
+	}
 
-    public String[] getSymbols() {
-        return symbols;
-    }
+	public Highchart setSymbols(Symbol[] pSymbols) {
+		String[] symbols = new String[pSymbols.length];
 
-    public Highchart setSymbols(String[] pSymbols) {
-        symbols = pSymbols;
-        return this;
-    }
+		for (int i = 0; i < symbols.length; i++) {
+			symbols[i] = pSymbols[i].toString();
+		}
 
-    public Highchart setSymbols(Symbol[] pSymbols) {
-        String[] symbols = new String[pSymbols.length];
+		return setSymbols(symbols);
+	}
 
-        for (int i = 0; i < symbols.length; i++) {
-            symbols[i] = pSymbols[i].toString();
-        }
+	public ChartTitle getTitle() {
+		return title;
+	}
 
-        return setSymbols(symbols);
-    }
+	public Highchart setTitle(ChartTitle pTitle) {
+		title = pTitle;
+		return this;
+	}
 
-    public ChartTitle getTitle() {
-        return title;
-    }
+	public Tooltip getTooltip() {
+		return tooltip;
+	}
 
-    public Highchart setTitle(ChartTitle pTitle) {
-        title = pTitle;
-        return this;
-    }
+	public Highchart setTooltip(Tooltip pTooltip) {
+		tooltip = pTooltip;
+		return this;
+	}
 
-    public Tooltip getTooltip() {
-        return tooltip;
-    }
+	@JsonProperty("xAxis")
+	public Axis getXAxis() {
+		return xAxis;
+	}
 
-    public Highchart setTooltip(Tooltip pTooltip) {
-        tooltip = pTooltip;
-        return this;
-    }
+	public Highchart setXAxis(Axis pXAxis) {
+		xAxis = pXAxis;
+		return this;
+	}
 
-    @JsonProperty("xAxis")
-    public Axis getXAxis() {
-        return xAxis;
-    }
+	@JsonProperty("yAxis")
+	public Axis getYAxis() {
+		return yAxis;
+	}
 
-    public Highchart setXAxis(Axis pXAxis) {
-        xAxis = pXAxis;
-        return this;
-    }
+	public Highchart setYAxis(Axis pYAxis) {
+		yAxis = pYAxis;
+		return this;
+	}
 
-    @JsonProperty("yAxis")
-    public Axis getYAxis() {
-        return yAxis;
-    }
+	public Exporting getExporting() {
+		return exporting;
+	}
 
-    public Highchart setYAxis(Axis pYAxis) {
-        yAxis = pYAxis;
-        return this;
-    }
+	public Highchart setExporting(Exporting pExporting) {
+		exporting = pExporting;
+		return this;
+	}
 
-    public Exporting getExporting() {
-        return exporting;
-    }
+	public Navigation getNavigation() {
+		return navigation;
+	}
 
-    public Highchart setExporting(Exporting pExporting) {
-        exporting = pExporting;
-        return this;
-    }
-
-    public Navigation getNavigation() {
-        return navigation;
-    }
-
-    public Highchart setNavigation(Navigation pNavigation) {
-        navigation = pNavigation;
-        return this;
-    }
+	public Highchart setNavigation(Navigation pNavigation) {
+		navigation = pNavigation;
+		return this;
+	}
 }
